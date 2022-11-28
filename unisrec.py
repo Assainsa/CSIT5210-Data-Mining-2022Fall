9ebd7f61585f064137c0e894f9f51c0cd6a2bde2
no77312 <65014504+no77312@users.noreply.github.com>
2022-11-28 22:37:10 +0800

    Add files via upload

--- unisrec.py
+++ unisrec.py
@@ -8,6 +8,7 @@ from recbole.model.sequential_recommender.sasrec import SASRec
 class PWLayer(nn.Module):
     """Single Parametric Whitening Layer
     """
+
     def __init__(self, input_size, output_size, dropout=0.0):
         super(PWLayer, self).__init__()
 
@@ -28,6 +29,7 @@ class PWLayer(nn.Module):
 class MoEAdaptorLayer(nn.Module):
     """MoE-enhanced Adaptor
     """
+
     def __init__(self, n_exps, layers, dropout=0.0, noise=True):
         super(MoEAdaptorLayer, self).__init__()
 
@@ -52,8 +54,8 @@ class MoEAdaptorLayer(nn.Module):
         return gates
 
     def forward(self, x):
-        gates = self.noisy_top_k_gating(x, self.training) # (B, n_E)
-        expert_outputs = [self.experts[i](x).unsqueeze(-2) for i in range(self.n_exps)] # [(B, 1, D)]
+        gates = self.noisy_top_k_gating(x, self.training)  # (B, n_E)
+        expert_outputs = [self.experts[i](x).unsqueeze(-2) for i in range(self.n_exps)]  # [(B, 1, D)]
         expert_outputs = torch.cat(expert_outputs, dim=-2)
         multiple_outputs = gates.unsqueeze(-1) * expert_outputs
         return multiple_outputs.sum(dim=-2)
@@ -66,6 +68,30 @@ class UniSRec(SASRec):
         self.train_stage = config['train_stage']
         self.temperature = config['temperature']
         self.lam = config['lambda']
+
+        self.trm_output_dim = config['adaptor_layers'][1]
+        self.momentum = 0.99
+        self.use_mlp = False
+        # for moco, set momentum_trm_encoder
+        if self.use_mlp:
+            self.prject_head = nn.Sequential(nn.Linear(self.trm_output_dim, 128), nn.ReLU())
+            self.query_encoder = nn.Sequential(self.trm_encoder, self.project_head)
+        else:
+            self.query_encoder = self.trm_encoder
+
+        self.key_encoder = copy.deepcopy(self.query_encoder)
+        # create queue (D, K)， K = 2^16, D is the output dim of transformer, K is the length of queue
+        self.queue_len = 65536
+        # seq_queue to store negative sequence representation
+        self.seq_queue = torch.randn(self.trm_output_dim, self.queue_len)
+        self.seq_queue = F.normalize(self.seq_queue, dim=0)
+        # seq_ queue to store negative sequence representation
+        self.item_queue = torch.randn(self.trm_output_dim, self.queue_len)
+        self.item_queue = F.normalize(self.item_queue, dim=0)
+        # point to next position to be inserted into the seq_queue
+        self.seq_queue_ptr = 0
+        # point to next position to be inserted into the item_queue
+        self.item_queue_ptr = 0
 
         assert self.train_stage in [
             'pretrain', 'inductive_ft', 'transductive_ft'
@@ -83,6 +109,47 @@ class UniSRec(SASRec):
             config['adaptor_layers'],
             config['adaptor_dropout_prob']
         )
+
+    @torch.no_grad()
+    def _seq_dequeue_and_enqueue(self, keys):
+        # gather keys before updating queue
+        batch_size = keys.shape[0]
+        ptr = self.seq_queue_ptr
+
+        if(ptr + batch_size > self.queue_len):
+            batch_size = self.queue_len - ptr
+
+
+        # replace the keys at ptr (dequeue and enqueue) 
+        self.seq_queue[:, ptr:ptr + batch_size] = keys.T
+        ptr = (ptr + batch_size) % self.queue_len
+
+        # move pointer
+        self.seq_queue_ptr = ptr
+
+    @torch.no_grad()
+    def _item_dequeue_and_enqueue(self, keys):
+        # gather keys before updating queue
+        batch_size = keys.shape[0]
+        ptr = self.item_queue_ptr
+
+        if(ptr + batch_size > self.queue_len):
+            batch_size = self.queue_len - ptr
+
+        # replace the keys at ptr (dequeue and enqueue)
+        self.item_queue[:, ptr:ptr + batch_size] = keys.T
+        ptr = (ptr + batch_size) % self.queue_len
+
+        # move pointer
+        self.item_queue_ptr = ptr
+
+    @torch.no_grad()
+    def _momentum_update_key_encoder(self):
+        """
+        Momentum update of the key encoder
+        """
+        for param_q, param_k in zip(self.query_encoder.parameters(), self.key_encoder.parameters()):
+            param_k.data = param_k.data * self.momentum + param_q.data * (1. - self.momentum)
 
     def forward(self, item_seq, item_emb, item_seq_len):
         position_ids = torch.arange(item_seq.size(1), dtype=torch.long, device=item_seq.device)
@@ -102,6 +169,42 @@ class UniSRec(SASRec):
         output = self.gather_indexes(output, item_seq_len - 1)
         return output  # [B H]
 
+    def query_forward(self, item_seq, item_emb, item_seq_len):
+        assert self.train_stage == 'pretrain'
+        position_ids = torch.arange(item_seq.size(1), dtype=torch.long, device=item_seq.device)
+        position_ids = position_ids.unsqueeze(0).expand_as(item_seq)
+        position_embedding = self.position_embedding(position_ids)
+
+        input_emb = item_emb + position_embedding
+        input_emb = self.LayerNorm(input_emb)
+        input_emb = self.dropout(input_emb)
+
+        extended_attention_mask = self.get_attention_mask(item_seq)
+
+        trm_output = self.query_encoder(input_emb, extended_attention_mask, output_all_encoded_layers=True)
+        output = trm_output[-1]
+        output = self.gather_indexes(output, item_seq_len - 1)
+        return output  # [B H]
+
+    # @torch.no_grad
+    def key_forward(self, item_seq, item_emb, item_seq_len):
+        assert self.train_stage == "pretrain"
+        position_ids = torch.arange(item_seq.size(1), dtype=torch.long, device=item_seq.device)
+        position_ids = position_ids.unsqueeze(0).expand_as(item_seq)
+        position_embedding = self.position_embedding(position_ids)
+
+        input_emb = item_emb + position_embedding
+
+        input_emb = self.LayerNorm(input_emb)
+        input_emb = self.dropout(input_emb)
+
+        extended_attention_mask = self.get_attention_mask(item_seq)
+
+        trm_output = self.key_encoder(input_emb, extended_attention_mask, output_all_encoded_layers=True)
+        output = trm_output[-1]
+        output = self.gather_indexes(output, item_seq_len - 1)
+        return output  # [B H] H = D
+
     def seq_item_contrastive_task(self, seq_output, same_pos_id, interaction):
         pos_items_emb = self.moe_adaptor(interaction['pos_item_emb'])
         pos_items_emb = F.normalize(pos_items_emb, dim=1)
@@ -110,44 +213,73 @@ class UniSRec(SASRec):
         pos_logits = torch.exp(pos_logits)
 
         neg_logits = torch.matmul(seq_output, pos_items_emb.transpose(0, 1)) / self.temperature
-        neg_logits = torch.where(same_pos_id, torch.tensor([0], dtype=torch.float, device=same_pos_id.device), neg_logits)
+        neg_logits = torch.where(same_pos_id, torch.tensor([0], dtype=torch.float, device=same_pos_id.device),
+                                 neg_logits)
         neg_logits = torch.exp(neg_logits).sum(dim=1)
 
         loss = -torch.log(pos_logits / neg_logits)
         return loss.mean()
 
-    def seq_seq_contrastive_task(self, seq_output, same_pos_id, interaction):
-        item_seq_aug = interaction[self.ITEM_SEQ + '_aug']
-        item_seq_len_aug = interaction[self.ITEM_SEQ_LEN + '_aug']
-        item_emb_list_aug = self.moe_adaptor(interaction['item_emb_list_aug'])
-        seq_output_aug = self.forward(item_seq_aug, item_emb_list_aug, item_seq_len_aug)
-        seq_output_aug = F.normalize(seq_output_aug, dim=1)
+    def seq_item_moco(self, seq_output, interaction):
+        pos_items_emb = self.moe_adaptor(interaction['pos_item_emb'])
+        pos_items_emb = F.normalize(pos_items_emb, dim=1)
 
-        pos_logits = (seq_output * seq_output_aug).sum(dim=1, keepdim=True) / self.temperature
+        pos_logits = (seq_output * pos_items_emb).sum(dim=1, keepdim=True) / self.temperature
         pos_logits = torch.exp(pos_logits)
 
-        neg_logits = torch.matmul(seq_output, seq_output_aug.transpose(0, 1)) / self.temperature
-        neg_logits = torch.where(same_pos_id, torch.tensor([0], dtype=torch.float, device=same_pos_id.device), neg_logits)
+        neg_logits = torch.matmul(seq_output, self.item_queue.clone().cuda().detach()) / self.temperature
         neg_logits = torch.exp(neg_logits).sum(dim=1)
 
         loss = -torch.log(pos_logits / neg_logits)
+
+        # insert into queue
+        self._item_dequeue_and_enqueue(pos_items_emb)
+        return loss.mean()
+
+    def seq_seq_moco(self, q_seq_output, interaction):
+        item_seq_aug = interaction[self.ITEM_SEQ + '_aug']
+        item_seq_len_aug = interaction[self.ITEM_SEQ_LEN + '_aug']
+        item_emb_list_aug = self.moe_adaptor(interaction['item_emb_list_aug'])
+
+        # calculate the positive key output for query (B, D), B is Batch Size and D is the output dim of transformer
+        k_seq_output = self.key_forward(item_seq_aug, item_emb_list_aug, item_seq_len_aug)
+        k_seq_output = F.normalize(k_seq_output, dim=1)
+
+        # (B, D) * (B, D) -> (B)
+        pos_logits = (q_seq_output * k_seq_output).sum(dim=1, keepdim=True) / self.temperature
+        pos_logits = torch.exp(pos_logits)
+
+        # (B, D) * (D, K) -> (B, K)
+        neg_logits = torch.matmul(q_seq_output, self.seq_queue.clone().cuda().detach()) / self.temperature
+        neg_logits = torch.exp(neg_logits).sum(dim=1)
+
+        loss = -torch.log(pos_logits / neg_logits)
+
+        # insert into queue
+        self._seq_dequeue_and_enqueue(k_seq_output)
+
         return loss.mean()
 
     def pretrain(self, interaction):
         item_seq = interaction[self.ITEM_SEQ]
         item_seq_len = interaction[self.ITEM_SEQ_LEN]
         item_emb_list = self.moe_adaptor(interaction['item_emb_list'])
-        seq_output = self.forward(item_seq, item_emb_list, item_seq_len)
-        seq_output = F.normalize(seq_output, dim=1)
+
+        # calculate the query output q (B, D) (256 300)
+        q_seq_output = self.query_forward(item_seq, item_emb_list, item_seq_len)
+        q_seq_output = F.normalize(q_seq_output, dim=1)
 
         # Remove sequences with the same next item
         pos_id = interaction['item_id']
         same_pos_id = (pos_id.unsqueeze(1) == pos_id.unsqueeze(0))
         same_pos_id = torch.logical_xor(same_pos_id, torch.eye(pos_id.shape[0], dtype=torch.bool, device=pos_id.device))
 
-        loss_seq_item = self.seq_item_contrastive_task(seq_output, same_pos_id, interaction)
-        loss_seq_seq = self.seq_seq_contrastive_task(seq_output, same_pos_id, interaction)
+        loss_seq_item = self.seq_item_moco(q_seq_output, interaction)
+        loss_seq_seq = self.seq_seq_moco(q_seq_output, interaction)
         loss = loss_seq_item + self.lam * loss_seq_seq
+
+        # update momentum encoder (key encoder)
+        self._momentum_update_key_encoder()
         return loss
 
     def calculate_loss(self, interaction):
